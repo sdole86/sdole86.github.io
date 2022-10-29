3f2ae17569eea67c267b5d6d7998fb01422b5207
sdole86 <92174592+sdole86@users.noreply.github.com>
2022-10-28 22:35:38 -0400

    Commit update

--- js/project.js
+++ js/project.js
@@ -1,145 +1,18 @@
-$(function() {
-    $( "#accordion_js" ).accordion({
-      collapsible: true,
-      active: false
-    })
-  });
 
-  $(function() {
-    $( "#accordion_react" ).accordion({
-      collapsible: true,
-      active: false
-    })
-  });
+let technology_id;
 
-$(function() {
-$( "#accordion_web" ).accordion({
-    collapsible: true,
-    active: false
-    })
-});
+function displayProjectSlideShow(selectedTechnology) {
+    console.log("display function called");
+    hideProjectSlideShow();
+    document.getElementById(selectedTechnology).style.display = 'block';
 
-$(function() {
-    $( "#accordion_cs" ).accordion({
-      collapsible: true,
-      active: false
-    })
-});
 
-$(function() {
-    $( "#accordion_asp" ).accordion({
-      collapsible: true,
-      active: false
-    })
-});
-
-$(function() {
-    $( "#accordion_python" ).accordion({
-      collapsible: true,
-      active: false
-    })
-});
-
-(function( $, undefined ) {
-
-$.widget( "ab.accordion", $.ui.accordion, {
-
-    refresh: function() {
-
-        this._super( "refresh" );
-
-        if ( this.options.heightStyle !== "content" ) {
-            return;
-        }
-
-        this.headers.next().each( function() {
-
-            if ( $( this ).css( "height" ) ) {
-                $( this ).css( "height", "" );
-            }
-
-        });
-
-    }
-
-});
-
-})(jQuery);
-
-$(function() {
-
-    $( "#accordion_js" ).accordion();
-
-    for ( var i=0; i<20; i++ ){
-        $( "ul" ).append( "<li>nth item</li>" );
-    }
-
-    $( "#accordion_js" ).accordion( "option", "heightStyle", "content" )
-                    .accordion( "refresh" );
-
-});
-
-$(function() {
-
-$( "#accordion_react" ).accordion();
-
-for ( var i=0; i<20; i++ ){
-    $( "ul" ).append( "<li>nth item</li>" );
 }
 
-$( "#accordion_react" ).accordion( "option", "heightStyle", "content" )
-                .accordion( "refresh" );
-
-});
-
-$(function() {
-
-    $( "#accordion_cs" ).accordion();
-
-    for ( var i=0; i<20; i++ ){
-        $( "ul" ).append( "<li>nth item</li>" );
+function hideProjectSlideShow() {
+    console.log("hide function called");
+    const select_classes = document.getElementsByClassName('slideshow-container');
+    for (const select_class of select_classes){
+        select_class.style.display = 'none';
     }
-
-    $( "#accordion_cs" ).accordion( "option", "heightStyle", "content" )
-                    .accordion( "refresh" );
-
-});
-
-$(function() {
-
-    $( "#accordion_python" ).accordion();
-
-    for ( var i=0; i<20; i++ ){
-        $( "ul" ).append( "<li>nth item</li>" );
-    }
-
-    $( "#accordion_cs" ).accordion( "option", "heightStyle", "content" )
-                    .accordion( "refresh" );
-
-});
-
-$(function() {
-
-    $( "#accordion_web" ).accordion();
-
-    for ( var i=0; i<20; i++ ){
-        $( "ul" ).append( "<li>nth item</li>" );
-    }
-
-    $( "#accordion_web" ).accordion( "option", "heightStyle", "content" )
-                    .accordion( "refresh" );
-
-});
-
-$(function() {
-
-    $( "#accordion_asp" ).accordion();
-
-    for ( var i=0; i<20; i++ ){
-        $( "ul" ).append( "<li>nth item</li>" );
-    }
-
-    $( "#accordion_asp" ).accordion( "option", "heightStyle", "content" )
-                    .accordion( "refresh" );
-
-});
+}
