1373e888492a24a9053a3db72a69f0746f7e088d
sdole86 <92174592+sdole86@users.noreply.github.com>
2022-11-19 16:55:39 -0500

    updated projects

--- js/project.js
+++ js/project.js
@@ -2,15 +2,11 @@
 let technology_id;
 
 function displayProjectSlideShow(selectedTechnology) {
-    console.log("display function called");
     hideProjectSlideShow();
     document.getElementById(selectedTechnology).style.display = 'block';
-
-
 }
 
 function hideProjectSlideShow() {
-    console.log("hide function called");
     const select_classes = document.getElementsByClassName('slideshow-container');
     for (const select_class of select_classes){
         select_class.style.display = 'none';
--- js/slideshow.js
+++ js/slideshow.js
@@ -1,11 +1,12 @@
-let slideIndex = [1,1,1,1,1];
+let slideIndex = [1,1,1,1,1,1];
 /* Class the members of each slideshow group with different CSS classes */
-let slideId = ["mySlides1", "mySlides2", "mySlides3", "mySlides4", "mySlides5"]
+let slideId = ["mySlides1", "mySlides2", "mySlides3", "mySlides4", "mySlides5", "mySlides6"]
 showSlides(1, 0);
 showSlides(1, 1);
 showSlides(1, 2);
 showSlides(1, 3);
 showSlides(1, 4);
+showSlides(1, 5);
 
 function plusSlides(n, no) {
   showSlides(slideIndex[no] += n, no);
